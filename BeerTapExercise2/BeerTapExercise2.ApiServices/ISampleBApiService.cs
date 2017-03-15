9f8735fa363e1bd18ca910b1131409bb3d2dfdd6
Nikki.Barros <Nikki.Barros@MAN42-W541-W10>
2017-03-15 13:44:17 +0800

    Add ISampleBApiService.cs

---
+++ BeerTapExercise2/BeerTapExercise2.ApiServices/ISampleBApiService.cs
@@ -0,0 +1,14 @@
+﻿using BeerTapExercise2.Model;
+using IQ.Platform.Framework.WebApi;
+
+namespace BeerTapExercise2.ApiServices
+{
+	public interface ISampleBApiService :
+		IGetAResourceAsync<SampleResource, string>,
+		IGetManyOfAResourceAsync<SampleResource, string>,
+		ICreateAResourceAsync<SampleResource, string>,
+		IUpdateAResourceAsync<SampleResource, string>,
+		IDeleteResourceAsync<SampleResource, string>
+	{
+	}
+}
